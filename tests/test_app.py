e34d93c422bf05a6634322f2cd7a48e75885fc05
Adnan Shaldrake <ashaldrake@gmail.com>
2022-10-05 19:05:45 +0530

    converted for pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,20 +1,24 @@
-import unittest
 import json
 
 from source import app
 
 
-class TestHandler(unittest.TestCase):
-    def test_base(self):
-        """
-        Test that it can sum a list of integers
-        """
-        event = {}
-        context = {}
-        result = app.handler(event, context)
-        base_result = {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
-        self.assertEqual(result, base_result)
+def test_base():
+    """
+    Test that it can sum a list of integers
+    """
+    event = {}
+    context = {}
+    result = app.handler(event, context)
+    base_result = {"statusCode": 20, "body": json.dumps("Hello from Lambda!")}
+    assert result == base_result
 
-
-if __name__ == "__main__":
-    unittest.main()
+def test_base2():
+    """
+    Test that it can sum a list of integers
+    """
+    event = {}
+    context = {}
+    result = app.handler(event, context)
+    base_result = {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    assert result == base_result
